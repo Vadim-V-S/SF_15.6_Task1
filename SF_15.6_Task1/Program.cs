4e9242d4389295aca424a9edf224b70723a72e9c
Vadim-V-S <vaddimsav@gmail.com>
2021-07-21 14:07:37 +0500

    Добавьте файлы проекта.

---
+++ SF_15.6_Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+
+
+
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+
+            //return (from cl in classes from st in cl.Students select st).ToArray();
+            return classes.SelectMany(x => x.Students).ToArray();
+
+        }
+
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
